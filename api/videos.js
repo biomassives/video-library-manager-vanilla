6f85021d62ee77fb14df504ce2699274f93787d1
G. Willson <greg@ecocity.com>
2024-11-24 12:21:48 -0700

    fix cors for api videos.js

--- api/videos.js
+++ api/videos.js
@@ -17,6 +17,10 @@ export default async function handler(req, res) {
 
   // Handle preflight request
   if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  
+
     return res.status(200).end();
   }
 
