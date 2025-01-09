530343a61ebc4e4b2fa276b238ab6401291b41f3
G. Willson <greg@ecocity.com>
2025-01-09 11:06:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -892,9 +892,9 @@ class CategoryManager {
           'py-2',
           'text-sm',
           'text-gray-700',
-          'dark:text-gray-200',
+          'dark:text-gray-700',
           'hover:bg-gray-100',
-          'dark:hover:bg-gray-700',
+          'dark:hover:bg-gray-100',
           'transition-colors'
         );
       });
